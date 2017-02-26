fa1db390fe3d531c6ca492f2fa870a4a78932a2b
R3aptor <rust.arabov@gmail.com>
2017-02-26 19:47:19 +0300

    Барометр

---
+++ barometer.js
@@ -0,0 +1,8 @@
+I2C1.setup({sda: SDA, scl: SCL, bitrate: 400000});
+var baro = require('@amperka/barometer').connect({i2c: I2C1});
+var disp = require('@amperka/quaddisplay').connect(P13);
+ 
+setInterval(function(){
+  disp.display(baro.read('mmHg').toFixed(0));
+
+},1000);
--- termometer.js
+++ termometer.js
@@ -1,10 +1,16 @@
 var term = require('@amperka/thermometer').connect(A4);
 var disp = require('@amperka/quaddisplay').connect(P13);
 var sum=0;
+I2C1.setup({sda: SDA, scl: SCL, bitrate: 400000});
+var bar = require('@amperka/barometer').connect({i2c: I2C1});
+var but = require('@amperka/button').connect(P0);
 disp.display('0000');
+bar.init();
 setInterval(function() {
   var cel = term.read('C');
+  var mm = bar.read('mmHg');
   sum=sum+parseFloat(cel.toFixed(1));
+  console.log(mm);
   
 }, 1000);
 setInterval(function(){
