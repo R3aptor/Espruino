54f9fd69eaaebdb5c7c0451304e218446f8935bf
R3aptor <rust.arabov@gmail.com>
2017-02-26 18:47:34 +0300

    Вывод на дисплей

--- termometer.js
+++ termometer.js
@@ -1,14 +1,14 @@
 var term = require('@amperka/thermometer').connect(A4);
 var disp = require('@amperka/quaddisplay').connect(P13);
 var sum=0;
+disp.display('0000');
 setInterval(function() {
   var cel = term.read('C');
-  
   sum=sum+parseFloat(cel.toFixed(1));
-  console.log(sum);
+  
 }, 1000);
 setInterval(function(){
   sum=sum/30;
-  console.log(sum.toFixed(1));
+  disp.display(sum.toFixed(1));
   sum=0;
 } ,30000);
